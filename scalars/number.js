9e05e96f123aa544ffdc5106fa3eee1ea0dae343
James Mortemore <jamesmortemore@gmail.com>
2018-06-03 16:16:20 +0100

    Support parseLiteral

--- scalars/number.js
+++ scalars/number.js
@@ -11,39 +11,47 @@ module.exports = class ConstraintNumberType extends GraphQLScalarType {
       parseValue (value) {
         value = type.serialize(value)
 
-        if (args.min && value < args.min) {
-          throw new ValidationError(fieldName,
-            `Must be at least ${args.min}`,
-            [{ arg: 'min', value: args.min }])
-        }
-        if (args.max && value > args.max) {
-          throw new ValidationError(fieldName,
-            `Must be no greater than ${args.max}`,
-            [{ arg: 'max', value: args.max }])
-        }
-
-        if (args.exclusiveMin && value <= args.exclusiveMin) {
-          throw new ValidationError(fieldName,
-            `Must be greater than ${args.exclusiveMin}`,
-            [{ arg: 'exclusiveMin', value: args.exclusiveMin }])
-        }
-        if (args.exclusiveMax && value >= args.exclusiveMax) {
-          throw new ValidationError(fieldName,
-            `Must be no greater than ${args.exclusiveMax}`,
-            [{ arg: 'exclusiveMax', value: args.exclusiveMax }])
-        }
-
-        if (args.multipleOf && value % args.multipleOf !== 0) {
-          throw new ValidationError(fieldName,
-            `Must be a multiple of ${args.multipleOf}`,
-            [{ arg: 'multipleOf', value: args.multipleOf }])
-        }
+        validate(fieldName, args, value)
 
         return type.parseValue(value)
       },
       parseLiteral (ast) {
-        return type.parseLiteral(ast)
+        const value = type.parseLiteral(ast)
+
+        validate(fieldName, args, value)
+
+        return value
       }
     })
+  }
+}
+
+function validate (fieldName, args, value) {
+  if (args.min && value < args.min) {
+    throw new ValidationError(fieldName,
+      `Must be at least ${args.min}`,
+      [{ arg: 'min', value: args.min }])
+  }
+  if (args.max && value > args.max) {
+    throw new ValidationError(fieldName,
+      `Must be no greater than ${args.max}`,
+      [{ arg: 'max', value: args.max }])
+  }
+
+  if (args.exclusiveMin && value <= args.exclusiveMin) {
+    throw new ValidationError(fieldName,
+      `Must be greater than ${args.exclusiveMin}`,
+      [{ arg: 'exclusiveMin', value: args.exclusiveMin }])
+  }
+  if (args.exclusiveMax && value >= args.exclusiveMax) {
+    throw new ValidationError(fieldName,
+      `Must be no greater than ${args.exclusiveMax}`,
+      [{ arg: 'exclusiveMax', value: args.exclusiveMax }])
+  }
+
+  if (args.multipleOf && value % args.multipleOf !== 0) {
+    throw new ValidationError(fieldName,
+      `Must be a multiple of ${args.multipleOf}`,
+      [{ arg: 'multipleOf', value: args.multipleOf }])
   }
 }
--- scalars/string.js
+++ scalars/string.js
@@ -13,70 +13,78 @@ module.exports = class ConstraintStringType extends GraphQLScalarType {
       parseValue (value) {
         value = type.serialize(value)
 
-        if (args.minLength && !isLength(value, { min: args.minLength })) {
-          throw new ValidationError(fieldName,
-            `Must be at least ${args.minLength} characters in length`,
-            [ { arg: 'minLength', value: args.minLength } ])
-        }
-        if (args.maxLength && !isLength(value, { max: args.maxLength })) {
-          throw new ValidationError(fieldName,
-            `Must be no more than ${args.maxLength} characters in length`,
-            [{ arg: 'maxLength', value: args.maxLength }])
-        }
-
-        if (args.startsWith && !value.startsWith(args.startsWith)) {
-          throw new ValidationError(fieldName,
-            `Must start with ${args.startsWith}`,
-            [{ arg: 'startsWith', value: args.startsWith }])
-        }
-
-        if (args.endsWith && !value.endsWith(args.endsWith)) {
-          throw new ValidationError(fieldName,
-            `Must end with ${args.endsWith}`,
-            [{ arg: 'endsWith', value: args.endsWith }])
-        }
-
-        if (args.contains && !contains(value, args.contains)) {
-          throw new ValidationError(fieldName,
-            `Must contain ${args.contains}`,
-            [{ arg: 'contains', value: args.contains }])
-        }
-
-        if (args.notContains && contains(value, args.notContains)) {
-          throw new ValidationError(fieldName,
-            `Must not contain ${args.notContains}`,
-            [{ arg: 'notContains', value: args.notContains }])
-        }
-
-        if (args.pattern && !new RegExp(args.pattern).test(value)) {
-          throw new ValidationError(fieldName,
-            `Must match ${args.pattern}`,
-            [{ arg: 'pattern', value: args.pattern }])
-        }
-
-        if (args.format) {
-          const formatter = formats[args.format]
-
-          if (!formatter) {
-            throw new ValidationError(fieldName,
-              `Invalid format type ${args.format}`,
-              [{ arg: 'format', value: args.format }])
-          }
-
-          try {
-            formatter(value) // Will throw if invalid
-          } catch (e) {
-            throw new ValidationError(fieldName,
-              e.message,
-              [{ arg: 'format', value: args.format }])
-          }
-        }
+        validate(fieldName, args, value)
 
         return type.parseValue(value)
       },
       parseLiteral (ast) {
-        return type.parseLiteral(ast)
+        const value = type.parseLiteral(ast)
+
+        validate(fieldName, args, value)
+
+        return value
       }
     })
+  }
+}
+
+function validate (fieldName, args, value) {
+  if (args.minLength && !isLength(value, { min: args.minLength })) {
+    throw new ValidationError(fieldName,
+      `Must be at least ${args.minLength} characters in length`,
+      [{ arg: 'minLength', value: args.minLength }])
+  }
+  if (args.maxLength && !isLength(value, { max: args.maxLength })) {
+    throw new ValidationError(fieldName,
+      `Must be no more than ${args.maxLength} characters in length`,
+      [{ arg: 'maxLength', value: args.maxLength }])
+  }
+
+  if (args.startsWith && !value.startsWith(args.startsWith)) {
+    throw new ValidationError(fieldName,
+      `Must start with ${args.startsWith}`,
+      [{ arg: 'startsWith', value: args.startsWith }])
+  }
+
+  if (args.endsWith && !value.endsWith(args.endsWith)) {
+    throw new ValidationError(fieldName,
+      `Must end with ${args.endsWith}`,
+      [{ arg: 'endsWith', value: args.endsWith }])
+  }
+
+  if (args.contains && !contains(value, args.contains)) {
+    throw new ValidationError(fieldName,
+      `Must contain ${args.contains}`,
+      [{ arg: 'contains', value: args.contains }])
+  }
+
+  if (args.notContains && contains(value, args.notContains)) {
+    throw new ValidationError(fieldName,
+      `Must not contain ${args.notContains}`,
+      [{ arg: 'notContains', value: args.notContains }])
+  }
+
+  if (args.pattern && !new RegExp(args.pattern).test(value)) {
+    throw new ValidationError(fieldName,
+      `Must match ${args.pattern}`,
+      [{ arg: 'pattern', value: args.pattern }])
+  }
+
+  if (args.format) {
+    const formatter = formats[args.format]
+
+    if (!formatter) {
+      throw new ValidationError(fieldName,
+        `Invalid format type ${args.format}`,
+        [{ arg: 'format', value: args.format }])
+    }
+
+    try {
+      formatter(value) // Will throw if invalid
+    } catch (e) {
+      throw new ValidationError(fieldName,
+        e.message,
+        [{ arg: 'format', value: args.format }])
+    }
   }
 }
