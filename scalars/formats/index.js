a17ca0086b094b102a3cc79c4de520f0e6d20f4f
Julien Richard <julien.richard@filigran.io>
2022-05-16 15:02:19 +0200

    fix: avoid dynamic require (#93)

--- scalars/formats/index.js
+++ scalars/formats/index.js
@@ -1,17 +1,21 @@
-const formats = [
-  'byte',
-  'date-time',
-  'date',
-  'email',
-  'ipv4',
-  'ipv6',
-  'uri',
-  'uuid'
-]
-const fns = {}
+const byte = require('./byte');
+const time = require('./date-time');
+const date = require('./date');
+const email = require('./email');
+const ipv4 = require('./ipv4');
+const ipv6 = require('./ipv6');
+const uri = require('./uri');
+const uuid = require('./uuid');
 
-formats.forEach(function (format) {
-  fns[format] = require('./' + format)
-})
+fns = {
+  byte,
+  'date-time': time,
+  date,
+  email,
+  ipv4,
+  ipv6,
+  uri,
+  uuid
+}
 
 module.exports = fns
