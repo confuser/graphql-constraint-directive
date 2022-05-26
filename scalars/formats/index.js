8b05c8ae47a2c47005907e0c467f2cfdb892fdce
Lukas Siemon <simlu@users.noreply.github.com>
2022-05-26 15:30:31 -0700

    fix: ensured compatibility with @vercel/ncc (#96)
    Co-authored-by: Lukas Siemon <lukas.siemon@blackflux.com>

--- scalars/formats/index.js
+++ scalars/formats/index.js
@@ -1,10 +1,19 @@
+const byte = require('./byte')
+const date = require('./date')
+const dateTime = require('./date-time')
+const email = require('./email')
+const ipv4 = require('./ipv4')
+const ipv6 = require('./ipv6')
+const uri = require('./uri')
+const uuid = require('./uuid')
+
 module.exports = {
-  byte: require('./byte'),
-  'date-time': require('./date-time'),
-  date: require('./date'),
-  email: require('./email'),
-  ipv4: require('./ipv4'),
-  ipv6: require('./ipv6'),
-  uri: require('./uri'),
-  uuid: require('./uuid')
+  byte,
+  'date-time': dateTime,
+  date,
+  email,
+  ipv4,
+  ipv6,
+  uri,
+  uuid
 }
