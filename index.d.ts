a1d9438cd067172daf0bcdc7ac707f7edd96e5c0
Yimi (César Vidril) <yimiprod@gmail.com>
2022-10-16 15:29:34 +0200

    fix: createApolloQueryValidationPlugin type (#109)
    fixes #107

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,5 @@
 import {GraphQLSchema, GraphQLError, DocumentNode, ValidationContext} from "graphql";
+import {PluginDefinition} from "apollo-server-core";
 import QueryValidationVisitor from "./lib/query-validation-visitor";
 
 export function constraintDirective () : (schema: GraphQLSchema) => GraphQLSchema;
@@ -7,7 +8,7 @@ export const constraintDirectiveTypeDefs: string
 
 export function validateQuery () : (schema: GraphQLSchema, query: DocumentNode, variables: Record<string, any>, operationName?: string) => Array<GraphQLError>;
 
-export function createApolloQueryValidationPlugin () : (schema: GraphQLSchema) => function;
+export function createApolloQueryValidationPlugin ( options: { schema: GraphQLSchema } ) : PluginDefinition;
 
 export function createQueryValidationRule( options: { [key: string]: any }) : (context: ValidationContext) => QueryValidationVisitor;
 
