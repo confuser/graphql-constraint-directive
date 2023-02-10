ad3164557075bb1f277966b2240415b5b33cc02a
Kat Schelonka <34227334+kschelonka@users.noreply.github.com>
2023-02-09 16:01:27 -0800

    fix(apollov4): add export from readme docs (#149)
    constraintDirectiveTypeDefsGql

--- apollo4.d.ts
+++ apollo4.d.ts
@@ -1,6 +1,8 @@
-import {GraphQLSchema} from "graphql";
+import {DocumentNode, GraphQLSchema} from "graphql";
 import {ApolloServerPlugin} from '@apollo/server';
 
-export const constraintDirectiveTypeDefs: string
+export const constraintDirectiveTypeDefs: string;
+
+export const constraintDirectiveTypeDefsGql: DocumentNode;
 
 export function createApollo4QueryValidationPlugin ( options: { schema: GraphQLSchema } ) : ApolloServerPlugin;
