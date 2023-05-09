f6335355f94c5a8087e365eca23701bc867f901a
jcoc611-gvt <131823963+jcoc611-gvt@users.noreply.github.com>
2023-05-08 18:20:31 -0700

    Missed return types

--- index.d.ts
+++ index.d.ts
@@ -1,14 +1,14 @@
 import {GraphQLSchema, GraphQLError, DocumentNode, ValidationContext} from "graphql";
-import {OperationDefinitionNode, FragmentDefinitionNode, InlineFragmentNode, FieldNode} from 'graphql/language';
+import {OperationDefinitionNode, FragmentDefinitionNode, InlineFragmentNode, FieldNode, ArgumentNode} from 'graphql/language';
 import {PluginDefinition} from "apollo-server-core";
 
 export class QueryValidationVisitor {
-    onOperationDefinitionEnter (operation: OperationDefinitionNode);
-    onFragmentEnter (node: FragmentDefinitionNode | InlineFragmentNode);
-    onFragmentLeave (node: FragmentDefinitionNode | InlineFragmentNode);
-    onFieldEnter (node: FieldNode);
-    onFieldLeave (node: FieldNode);
-    onArgumentEnter (arg: ArgumentNode);
+    onOperationDefinitionEnter (operation: OperationDefinitionNode): void;
+    onFragmentEnter (node: FragmentDefinitionNode | InlineFragmentNode): void;
+    onFragmentLeave (node: FragmentDefinitionNode | InlineFragmentNode): void;
+    onFieldEnter (node: FieldNode): void;
+    onFieldLeave (node: FieldNode): void;
+    onArgumentEnter (arg: ArgumentNode): void;
 }
 
 /**
