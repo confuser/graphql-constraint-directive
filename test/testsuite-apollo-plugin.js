0378c1d12fe37af219be85908e012e635927d17f
simon <simon@mosaicml.com>
2024-02-16 13:22:11 +0100

    Fix for trying to read args on undefined

--- test/testsuite-apollo-plugin.js
+++ test/testsuite-apollo-plugin.js
@@ -18,4 +18,5 @@ describe('Server validator based implementation - Apollo plugin', function () {
   require('./string.test').test(setup, IMPL_TYPE)
   require('./argument-dynamic.test').test(setup, IMPL_TYPE)
   require('./union.test').test(setup, IMPL_TYPE)
+  require('./foreign-query-directives.test').test(setup, IMPL_TYPE)
 })
